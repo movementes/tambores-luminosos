63f71f9fd93d25d7371144506f923ed9c5b4edff
MOVEMENTES <movementes@users.noreply.github.com>
2018-01-14 20:14:03 -0200

    Add files via upload

---
+++ piezo_caixa.ino
@@ -0,0 +1,65 @@
+const int ledPin1 = 2;
+const int ledPin2 = 3;
+const int ledPin3 = 4;
+const int ledPin4 = 5;
+const int ledPin5 = 6;
+const int ledPin6 = 7;
+const int ledPin7 = 8;
+const int ledPin8 = 9;
+const int ledPin9 = 10;
+const int ledPin10 = 11;
+const int ledPin11 = 12;
+const int ledPin12 = 13;
+const int knockSensor = A0;
+const int threshold = 60;
+
+int sensorReading = 0;
+int ledState = LOW;
+
+void setup() {
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin7, OUTPUT);
+  pinMode(ledPin8, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+  pinMode(ledPin10, OUTPUT);
+  pinMode(ledPin11, OUTPUT);
+  pinMode(ledPin12, OUTPUT);
+}
+
+void loop() {
+  sensorReading = analogRead(knockSensor);
+
+  if (sensorReading >= threshold) {
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, HIGH);
+    digitalWrite(ledPin6, HIGH);
+    digitalWrite(ledPin7, HIGH);
+    digitalWrite(ledPin8, HIGH);
+    digitalWrite(ledPin9, HIGH);
+    digitalWrite(ledPin10, HIGH);
+    digitalWrite(ledPin11, HIGH);
+    digitalWrite(ledPin12, HIGH);
+    delay(100);
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin4, LOW);
+    digitalWrite(ledPin5, LOW);
+    digitalWrite(ledPin6, LOW);
+    digitalWrite(ledPin7, LOW);
+    digitalWrite(ledPin8, LOW);
+    digitalWrite(ledPin9, LOW);
+    digitalWrite(ledPin10, LOW);
+    digitalWrite(ledPin11, LOW);
+    digitalWrite(ledPin12, LOW);
+    delay(100);
+  }
+}
---
+++ piezo_ilu.ino
@@ -0,0 +1,57 @@
+const int ledPin1 = 6;
+const int ledPin2 = 7;
+const int ledPin3 = 8;
+const int ledPin4 = 9;
+const int ledPin5 = 10;
+const int ledPin6 = 11;
+const int ledPin7 = 12;
+const int ledPin8 = 13;
+const int ledPin9 = 12;
+const int ledPin10 = 13;
+const int knockSensor = A0;
+const int threshold = 60;
+
+int sensorReading = 0;
+int ledState = LOW;
+
+void setup() {
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin7, OUTPUT);
+  pinMode(ledPin8, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+  pinMode(ledPin10, OUTPUT);
+}
+
+void loop() {
+  sensorReading = analogRead(knockSensor);
+
+  if (sensorReading >= threshold) {
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin4, HIGH);
+    digitalWrite(ledPin5, HIGH);
+    digitalWrite(ledPin6, HIGH);
+    digitalWrite(ledPin7, HIGH);
+    digitalWrite(ledPin8, HIGH);
+    digitalWrite(ledPin9, HIGH);
+    digitalWrite(ledPin10, HIGH);
+    delay(100);
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin4, LOW);
+    digitalWrite(ledPin5, LOW);
+    digitalWrite(ledPin6, LOW);
+    digitalWrite(ledPin7, LOW);
+    digitalWrite(ledPin8, LOW);
+    digitalWrite(ledPin9, LOW);
+    digitalWrite(ledPin10, LOW);
+    delay(100);
+  }
+}
